a25ddc4a2baebb6e89d68a3df8caddba4f05e716
Ben-20048 <20048@haurakiplains.school.nz>
2024-08-20 10:20:12 +1200

    map/movement in progress

--- Project_Renaissance.py
+++ Project_Renaissance.py
@@ -1,1 +1,26 @@
-import pygame
+import pygame, sys
+
+pygame.init()
+screen = pygame.display.set_mode((2432/3,2528/3))
+clock = pygame.time.Clock()
+
+bg_surface = pygame.image.load('Assets/Digital_assessment_map_no-light.png').convert()
+#bg_surface = pygame.transform.scale2x(bg_surface)
+#bg_surface = pygame.transform.scale_by(bg_surface, 0.333)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+           
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                pass
+     
+                
+    screen.blit(bg_surface,(0,0)) 
+    pygame.display.update()
+    clock.tick(120)
