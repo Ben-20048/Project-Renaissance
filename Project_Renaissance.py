78c4d19a2ef87e7c6141648fc16eb83dbfd72941
Ben-20048 <20048@haurakiplains.school.nz>
2024-08-29 14:20:58 +1200

    new collision system not owkring, uploading and trying somethng else new

--- Project_Renaissance.py
+++ Project_Renaissance.py
@@ -29,66 +29,86 @@ wall_list = [wall1,wall2]
 new_wall_list = []
 
 color = pygame.Color("yellow")
+coll = False
 
+move_right = True
+move_left = True
+move_up = True
+move_down = True
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-                      
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(pygame.mouse.get_pos())              
     keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_d]:
-        screen.fill(pygame.Color("black"))
-        screen.blit(bg_surface,(move_x,move_y))
-        move_x += -5
-        rand_rect.x += -5
-        for wall in wall_list:
-            wall.x += -5
-                
-
-    if keys[pygame.K_a]:
-        screen.fill(pygame.Color("black"))
-        screen.blit(bg_surface,(move_x + 10,move_y))
-        move_x += 5
-        rand_rect.x += 5
-        for wall in wall_list:
-            wall.x += 5
-        
-    if keys[pygame.K_w]:
-        screen.fill(pygame.Color("black"))
-        screen.blit(bg_surface,(move_x + 5,move_y + 5))
-        move_y += 5
-        rand_rect.y += 5
-        for wall in wall_list:
-            wall.y += 5
-        
-    if keys[pygame.K_s]:
-        screen.fill(pygame.Color("black"))
-        screen.blit(bg_surface,(move_x + 5,move_y - 5))
-        move_y += -5
-        rand_rect.y += -5
-        for wall in wall_list:
-            wall.y += -5
+    movement_speed = 5
+    
             
 
     for wall in wall_list:
 
         if char_rect.colliderect(wall):
             print ("collision")
-
+            coll = True
             if abs(char_rect.right - wall.left) <=10:
                 print("collision right")
-                bg_surface, (move_x - 5)
-                move_x += 5
-                rand_rect.x += 5
-                for wall in wall_list:
-                    new_wall_list.append(wall+1)
-                    
+                print(coll)
+                #bg_surface, (move_x - 5)
+                #move_x += 5
+                #rand_rect.x += 5
+                #for wall in wall_list:
+                 #   new_wall_list.append(wall.x+1)
+                char_rect.right = wall.left
+                movement_speed = 0
+                move_right = False
+            
+        if abs(char_rect.left - wall.right) <=10:
+            print("collision right")
+            print(coll)
+            char_rect.right = wall.left
+            movement_speed = 0
+            move_left = False
+                
+        else:
+            coll = False
+                
+    if keys[pygame.K_d] and move_right and not coll:
+        screen.fill(pygame.Color("black"))
+        screen.blit(bg_surface,(move_x,move_y))
+        move_x += -movement_speed
+        rand_rect.x += -movement_speed
+        for wall in wall_list:
+            wall.x += -movement_speed
+                
+
+    if keys[pygame.K_a] and move_left and not coll:
+        screen.fill(pygame.Color("black"))
+        screen.blit(bg_surface,(move_x + 10,move_y))
+        move_x += movement_speed
+        rand_rect.x += movement_speed
+        for wall in wall_list:
+            wall.x += movement_speed
         
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        print(pygame.mouse.get_pos())
+    if keys[pygame.K_w] and move_up and not coll:
+        screen.fill(pygame.Color("black"))
+        screen.blit(bg_surface,(move_x + 5,move_y + 5))
+        move_y += movement_speed
+        rand_rect.y += movement_speed
+        for wall in wall_list:
+            wall.y += movement_speed
+        
+    if keys[pygame.K_s] and move_down and not coll:
+        screen.fill(pygame.Color("black"))
+        screen.blit(bg_surface,(move_x + 5,move_y - 5))
+        move_y += -movement_speed
+        rand_rect.y += -movement_speed
+        for wall in wall_list:
+            wall.y += -movement_speed                
+        
+    
         
     for wall in wall_list:
         pygame.draw.rect(screen, color, wall)
