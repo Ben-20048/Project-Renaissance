f916fe6fa8cdec0a175c4aad5a19afaa23b42fbb
Ben-20048 <20048@haurakiplains.school.nz>
2024-08-30 10:14:34 +1200

    collisions are very close to working

--- Project_Renaissance.py
+++ Project_Renaissance.py
@@ -26,20 +26,24 @@ rand_rect = pygame.Rect(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,50,50)
 char_rect = pygame.Rect(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,50,50)
 
 #wall data
-wall1 = pygame.Rect(591,508,1000,370-235)
-wall2 = pygame.Rect(591,380,620,10-0)
+wall2 = pygame.Rect(591,508,1000,370-235)
+wall1 = pygame.Rect(591,380,620,10-0)
+wall3 = pygame.Rect(591, 100, 620, 5)
 
-wall_list = [wall1,wall2]
+wall_list = [wall1,wall2, wall3]
 new_wall_list = []
 
 color = pygame.Color("yellow")
 
+collision_detected = False
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(pygame.mouse.get_pos())
                       
     keys = pygame.key.get_pressed()
 
@@ -78,57 +82,69 @@ while True:
             
 
     for wall in wall_list:
-
         if char_rect.colliderect(wall):
-            print ("collision")
+            collision_detected = True
+            print ("collision with " + str(wall))
 
             if char_rect.right - wall.left <=20:
-                #print("collision right")
-                #print (char_rect.bottom)
-                #print (wall.top)
+                print("collision right")
+                print (char_rect.right)
+                print (wall.left)
+                print(char_rect.right - wall.left)
                 moving_right = 0
                 if char_rect.right - wall.left <=10:
-                    #print("uncollided right")
+                    print("uncollided right")
                     moving_right = -5
                     
-            if char_rect.left - wall.right >=-20:
-                #print("collision left")
+            elif char_rect.left - wall.right >=-20:
+                print("collision left")
                 #print(char_rect.left)
                 #print(wall.right)
                 moving_left = 0
                 if char_rect.left - wall.right >=-10:
-                    #print("uncollided left")
+                    print("uncollided left")
                     moving_left = 5
                     
-            if char_rect.bottom - wall.top <=20:
+            elif char_rect.bottom - wall.top <=20:
                 #print("collision bottom")
                 moving_down = 0
                 if char_rect.bottom - wall.top <=10:
                     #print("uncollided bottom")
                     moving_down = -5
                     
-            if char_rect.top - wall.bottom >=-20:
+            elif char_rect.top - wall.bottom >=-20:
                 #print("collision top")
                 moving_up = 0
                 if char_rect.top - wall.bottom >=-10:
                     #print("uncollided top")
                     moving_up = 5
-
-        if char_rect.bottom - wall.top <= -1:
-            #print("slide col exe")
-            moving_right = -5
-            moving_left = 5
-            
-        #if char_rect.top < wall.bottom:
-            #print("slide col exe 2")
-            #print(char_rect.top)
-            #print(wall.bottom)
+                    
+        #else:
             #moving_right = -5
             #moving_left = 5
+            #moving_up = 5
+            #moving_down = -5
+                    
+    if not collision_detected:
+        #print("slide col exe 2")
+        #print("char rect top: " + str(char_rect.top))
+        #print("wall bottom: " + str(wall.bottom))
+        #print("moving right: " + str(moving_right) + "          moving left: " + str(moving_left))
+        moving_right = -5
+        moving_left = 5
+        moving_up = 5
+        moving_down = -5
+
+        #if char_rect.bottom - wall.top <= -1:
+            #print("slide col exe")
+            #moving_right = -5
+            #moving_left = 5
+            
+            #if char_rect.top - wall.bottom <= -1:
+            
                     
         
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        print(pygame.mouse.get_pos())
+
         
     for wall in wall_list:
         pygame.draw.rect(screen, color, wall)
